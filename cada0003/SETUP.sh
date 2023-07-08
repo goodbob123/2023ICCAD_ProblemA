e582ba860b1516dc87598da8a28a38549da42c83
goodbob123 <johnsonsui3@gmail.com>
2023-07-08 15:47:16 +0800

    update SETUP.sh

--- cada0003/SETUP.sh
+++ cada0003/SETUP.sh
@@ -12,13 +12,13 @@ git reset 581c58b
 sed -e '/#include <cmath>/ a#include <stdexcept>' src/aig/gia/giaNewBdd.h > src/aig/gia/tempBdd.h
 sed -e '/#include <bitset>/ a#include <stdexcept>' src/aig/gia/giaNewTt.h > src/aig/gia/tempTt.h
 rm -rf src/aig/gia/giaNewBdd.h; rm -rf src/aig/gia/giaNewTt.h;
-mv src/aig/gia/tempBdd.h src/aig/gia/giaNewBdd.h; mv src/aig/gia/tempBdd.h src/aig/gia/giaNewBdd.h;
+mv src/aig/gia/tempBdd.h src/aig/gia/giaNewBdd.h; mv src/aig/gia/tempTt.h src/aig/gia/giaNewTt.h;
 
 # make "abc" static library
 make libabc.a
 
 # copy "libabc.a" static library to /lib
-mv libabc.a ../lib;
+mv libabc.a ../lib/libabc.a;
 
 # make bmatch
 cd ..; make;
