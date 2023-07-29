5bd16f240bc64b9aa7980f22529a8fd8734ce72c
陳韋旭 <wish@cthulhu.ee.ntu.edu.tw>
2023-07-29 22:02:39 +0800

    remove submodule

---
+++ src/SAT/glucose/simp/depend.mk
@@ -0,0 +1,41 @@
+/home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/Main.o /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/Main.or /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/Main.od /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/Main.op: /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/Main.cc \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../utils/System.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/IntTypes.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../utils/ParseUtils.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../utils/Options.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Vec.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/XAlloc.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../core/Dimacs.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../core/SolverTypes.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Alg.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Map.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Alloc.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../simp/SimpSolver.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Queue.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../core/Solver.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Heap.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../core/BoundedQueue.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../core/Constants.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Clone.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../core/SolverStats.h
+/home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/SimpSolver.o /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/SimpSolver.or /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/SimpSolver.od /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/SimpSolver.op: \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/SimpSolver.cc \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Sort.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Vec.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/IntTypes.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/XAlloc.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../simp/SimpSolver.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Queue.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../core/Solver.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Heap.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Alg.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../utils/Options.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../utils/ParseUtils.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../core/SolverTypes.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Map.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Alloc.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../core/BoundedQueue.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../core/Constants.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../mtl/Clone.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../core/SolverStats.h \
+ /home/wish/abcBug/2023ICCAD_ProblemA/src/SAT/glucose/simp/../utils/System.h
