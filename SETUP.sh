530a3e40c391c2044675d3980c5f6e284f0f1083
劉虹伶 <ips73519680@cthulhu.ee.ntu.edu.tw>
2023-07-30 13:57:56 +0800

    Revert "Merge branch 'develop' into partial"
    This reverts commit 25ab938193f6bde4996988d9f43e7d0938c26e8e, reversing
    changes made to 444a2322fffc2062e16785027fadb6769963f14a.

--- SETUP.sh
+++ SETUP.sh
@@ -19,9 +19,9 @@ sed -e '1618,1619 c \        Abc_NtkForEachCi( pNtk, pObj, k )\n            fpri
 sed -e '1620 c \        fprintf(file, "\\n" );' src/base/abci/abcPrint.c > src/base/abci/tempPrint.c
 sed -e '1625 a \    fclose(file);' src/base/abci/tempPrint.c > src/base/abci/abcPrint.c
 
-#rm -rf src/base/abc/tempDfs.c
-#sed -e '900 c \        if ( Abc_ObjIsCo(ppNodes[i]) && ppNodes[i]->vFanins.pArray[0] != 0 )' src/base/abc/abcDfs.c > src/base/abc/tempDfs.c
-#rm -rf src/base/abc/abcDfs.c; mv src/base/abc/tempDfs.c src/base/abc/abcDfs.c;
+rm -rf src/base/abc/tempDfs.c
+sed -e '900 c \        if ( Abc_ObjIsCo(ppNodes[i]) && ppNodes[i]->vFanins.pArray[0] != 0 )' src/base/abc/abcDfs.c > src/base/abc/tempDfs.c
+rm -rf src/base/abc/abcDfs.c; mv src/base/abc/tempDfs.c src/base/abc/abcDfs.c;
 
 # make "abc" static library
 make libabc.a
